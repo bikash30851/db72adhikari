c5ac9b7bafe9b7132bbcef9eb7efcf306e06be47
Bikash Adhikari <bikash30851@gmail.com>
2021-11-10 20:09:52 -0600

    Changes

--- app.js
+++ app.js
@@ -1,14 +1,65 @@
 var createError = require('http-errors');
+var mongoose=require('mongoose');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var Sweater = require("./models/Sweater");
+
+const connectionString =process.env.MONGO_CON;
+mongoose = require('mongoose');
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+var db= mongoose.connection;
+
+//Bind connection to error event
+db.on('error', console.error.bind(console, `MongoDB connection
+error:`));
+db.once("open", function(){
+
+
+console.log("Connection to DB succeeded")});
+
+// We can seed the collection if needed on
+//server start
+async function recreateDB() {
+  // Delete everything
+  await Sweater.deleteMany();
+  let instance1 = new Sweater({ size: "Small",
+    color: "White", price: 13});
+  instance1.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("First object saved");
+  });
+  let instance2 = new Sweater({
+    size: "Medium", color: "Red",
+    cost: 12});
+  instance2.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("Second object saved");
+  });
+  let instance3 = new Sweater({
+    size: "Large", color: "Green",
+    cost: 18});
+  instance3.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("Third object saved");
+  });
+}
+
+let reseed = true;
+if (reseed) { recreateDB(); }
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var SweaterRouter = require('./routes/Sweater');
 var addmodsRouter = require('./routes/addmods');
 var selectorRouter = require('./routes/selector');
+var resourceRouter = require("./routes/resource");
 
 var app = express();
 
@@ -27,15 +78,15 @@ app.use('/users', usersRouter);
 app.use('/Sweater', SweaterRouter);
 app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
-
+app.use("/resource", resourceRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
