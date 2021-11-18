f5d113ff7b8d5875576ea763ed17f2a45e0a7139
Bikash Adhikari <bikash30851@gmail.com>
2021-11-17 21:01:37 -0600

    made changes

--- controllers/Sweater.js
+++ controllers/Sweater.js
@@ -115,13 +115,13 @@ exports.Sweater_view_one_Page = async function(req, res) {
   }
   };
 
-    // Handle building the view for creating a flowers.
+    // Handle building the view for creating a Sweater.
   // No body, no in path parameter, no query.
   // Does not need to be async
-  exports.flowers_create_Page = function(req, res) {
+  exports.Sweater_create_Page = function(req, res) {
     console.log("create view")
     try{
-    res.render('flowerscreate', { title: 'flowers Create'});
+    res.render('Sweatercreate', { title: 'Sweater Create'});
     }
     catch(err){
     res.status(500)
