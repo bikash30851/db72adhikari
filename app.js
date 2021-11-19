bf61ba12ef0a6cf3175166b993509a564ea09aa7
Bikash Adhikari <bikash30851@gmail.com>
2021-11-19 10:51:37 -0600

    made changes

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var Sweater = require("./models/Sweater");
 
 const connectionString =process.env.MONGO_CON;
