18f68c80ba1b2f0d4b4e450ea360352fb1d4647c
Bikash Adhikari <bikash30851@gmail.com>
2021-11-30 20:23:17 -0600

    made changes

--- models/Sweater.js
+++ models/Sweater.js
@@ -4,7 +4,6 @@ const { SchemaTypes } = require('mongoose');
 const SweaterSchema = mongoose.Schema({
 size: String,
 color: String,
-price: Number
-})
+price: {type: Number, min:[5], max:[500] }})
 
 module.exports = mongoose.model("Sweater", SweaterSchema);
