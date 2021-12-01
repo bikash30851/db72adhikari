51eb4b42ec880c77eee2d965ab6d002d276adccc
Bikash Adhikari <bikash30851@gmail.com>
2021-11-30 20:00:33 -0600

    made changes

--- routes/Sweater.js
+++ routes/Sweater.js
@@ -3,9 +3,13 @@ const Sweater_controlers = require("../controllers/Sweater");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('Sweater', { title: 'Search Results Sweater' });
-});
+const secured = (req, res, next) => {
+  if (req.user){
+  return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+  }
 
 /* GET Sweater */
 router.get("/", Sweater_controlers.Sweater_view_all_Page);
